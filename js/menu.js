094410e3733b3c4e9f6a9ae4e87231d3e6203ff0
Fabian Johansson <fabulo1998@gmail.com>
2019-10-09 12:31:49 +0200

    improved styling, added boilerplate menu generation

--- js/menu.js
+++ js/menu.js
@@ -5,3 +5,29 @@ document.getElementsByClassName('close-btn')[0].addEventListener('click', showHi
 function showHideMenu() {
     document.getElementsByClassName('slide-out-menu')[0].classList.toggle('slide-out-menu-extended');
 }
+
+class MenuItem{
+    constructor(title) {
+        this.title = title;
+        this.container = document.createElement('div');
+        this.p = document.createElement('p');
+        this.icon = document.createElement('img');
+        this.arrow = 'pathToArrow';
+        this.childContainer = document.createElement('div');
+        this.children = [];
+    }
+
+    setup() {
+        this.container.classList.add('menu-item');
+        this.childContainer.classList.add('menu-item-children');
+        this.icon.setAttribute('src', this.arrow);
+    }
+
+    append(element) {
+        element.appendChild(this);
+    }
+
+    toggle() {
+        this.childContainer.classList.toggle('menu-item-')
+    }
+}
